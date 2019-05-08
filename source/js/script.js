5d49799b43cb93e285b11db8e5f1eb1f12073c50
DeepDownAmerican <Egor-Miass@yandex.ru>
2019-05-08 18:18:13 +0500

    небольшие правки после адаптивной графики

--- source/js/script.js
+++ source/js/script.js
@@ -13,16 +13,17 @@
     }
   });
 
-  var link = document.querySelector(".catalog__item-buy");
-  var popup = document.querySelector(".popup");
-  var close = popup.querySelector(".popup__add-cart");
+  // FIXME: не работает
+  // var link = document.querySelector(".catalog__item-buy");
+  // var popup = document.querySelector(".popup");
+  // var close = popup.querySelector(".popup__add-cart");
 
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("popup-show");
-  });
+  // link.addEventListener("click", function (evt) {
+  //   evt.preventDefault();
+  //   popup.classList.add("popup-show");
+  // });
 
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("popup-show");
-  });
+  // close.addEventListener("click", function (evt) {
+  //   evt.preventDefault();
+  //   popup.classList.remove("popup-show");
+  // });
