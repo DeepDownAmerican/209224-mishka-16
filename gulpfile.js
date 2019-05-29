51c90082a98de339d0c209fef2e825cfe067ba2e
DeepDownAmerican <Egor-Miass@yandex.ru>
2019-05-29 16:48:44 +0500

    Коммит перед сборкой

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ gulp.task("images", function () {
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
+    .pipe(webp({quality:}))
     .pipe(gulp.dest("source/img"))
 });
 
